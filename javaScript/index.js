33ba28f1a8c01fa1f12e96612bdd2af6c4678e62
Ahsan Habib <ahsanpub16b@gmail.com>
2022-03-10 20:05:15 +0600

    Weather api project working done here

--- javaScript/index.js
+++ javaScript/index.js
@@ -4,7 +4,7 @@ const API_KEY = `1c6cce3fefd6bf6b35e81913c0112772`;
 
 const searchTemperature = () => {
     const city = document.getElementById('city-name').value;
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
 
     fetch(url)
         .then(res => res.json())
@@ -13,6 +13,20 @@ const searchTemperature = () => {
 
 }
 
+const setInnerText = (id , text) => {
+    document.getElementById(id).innerText = text;
+
+}
+
 const displayTemperature = temperature => {
-    console.log(temperature);
+    // console.log(temperature);
+    setInnerText('city' , temperature.name);
+    setInnerText('temperature' , temperature.main.temp);
+    setInnerText('condition' , temperature.weather[0].main);
+
+    //set weather icon
+    const url = `http://openweathermap.org/img/wn/${temperature.weather[0].icon}@2x.png`;
+    const imgIcon = document.getElementById('weather-icon');
+    imgIcon.setAttribute('src' , url);
+    
 }
